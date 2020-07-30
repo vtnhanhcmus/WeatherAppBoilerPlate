080a40d023b2a4aa3a54fdee5d43562f31fcfd4d
nhanvo <vtnhanhcmus@gmail.com>
2020-07-30 14:48:59 +0700

    Init project

---
+++ src/main/java/com/weather/boilerplate/WeatherAppBoilerPlate/WeatherAppBoilerPlateApplication.java
@@ -0,0 +1,13 @@
+package com.weather.boilerplate.WeatherAppBoilerPlate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAppBoilerPlateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAppBoilerPlateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/weather/boilerplate/WeatherAppBoilerPlate/WeatherAppBoilerPlateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weather.boilerplate.WeatherAppBoilerPlate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAppBoilerPlateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
